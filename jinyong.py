e6c1a093610bf9993249b8bc4888a988a01f4448
rockz <zrc203@qq.com>
2019-06-14 17:51:48 +0800

    a

--- jinyong.py
+++ jinyong.py
@@ -7,29 +7,30 @@ from gen_opf import opf
 from gen_top_html import top_html
 from gen_content import content_html
 import time
+import random
 
 
 def get_content(url):
-    try:
-        time.sleep(3)
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"}
-        r = requests.request('get', url, headers=headers)
-        r.encoding = 'utf-8'
-        if r.status_code != 200:
-            print('***' + url)
-            return get_content(url)
-        soup = BeautifulSoup(r.text, 'lxml')
-        return soup
-    except BaseException:
-        traceback.print_exc()
-        return get_content(url)
+    while True:
+        try:
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"}
+            r = requests.request('get', url, headers=headers)
+            # time.sleep(random.randint(3, 4))
+            r.encoding = 'utf-8'
+            if r.status_code != 200:
+                print('***' + url)
+                continue
+            return BeautifulSoup(r.text, 'lxml')
+        except BaseException:
+            traceback.print_exc()
+            continue
 
 
-rootUrl = 'http://www.jinyongwang.com'
+xiuding = 'http://jinyongxiaoshuo.com/xiuding'
+rootUrl = 'http://jinyongxiaoshuo.com/'
 top_info = []
-urls = get_content(rootUrl).select('[class="book_li_other"] a')
-urls = filter(lambda url: url.text == '修订版', urls)
+urls = get_content(xiuding).select('dd a')
 root_path = 'D:\\jin\\修订版'
 ncx_list = []
 dc_info = dict()
@@ -44,19 +45,19 @@ for novel_url in urls:
         os.mkdir(root_path)
     top_soup = get_content(rootUrl + novel_url['href'])
     en_name = novel_url['href'].split('/')[-2]
-    book_name = top_soup.select_one('font[class="phonenone"]').text
+    book_name = novel_url.select_one('b').text
     print(book_name)
     top = {'book_title': book_name, 'book_url': en_name + '.html', 'child_tops': []}
     ncx_list.append({'text': book_name, 'src': en_name + '.html'})
     item_info.append({'id': en_name, 'href': en_name + '.html'})
     chap_list = []
     i = 1
-    top_soup_list = top_soup.select('[class="mlist"] a')
+    top_soup_list = top_soup.select('dd a')
     if book_name.startswith('射雕英雄') or book_name.startswith('雪山飞狐'):
         top_soup_list.reverse()
     for chap_url_soup in top_soup_list:
         chap_name = chap_url_soup.text
-        content_soup = get_content(rootUrl + chap_url_soup['href'])
+        content_soup = get_content(chap_url_soup['href'])
         chap = dict()
         chap['chap_name'] = chap_name
         child_top = {'chap_url': chap_name, 'chap_title': en_name + '.html#%s' % i}
@@ -64,12 +65,16 @@ for novel_url in urls:
         ncx_list.append({'text': chap_name, 'src': en_name + '.html#%s' % i})
         print(chap_name)
         chap_content = []
-        for chap_soup in content_soup.select('[id="vcon"] p'):
+        content_list = content_soup.select('div[class="entry"] p')
+        for chap_soup in content_list:
+            if 'JinYongXiaoShuo' in chap_soup.text:
+                break
             chap_content.append(chap_soup.text)
         chap['content'] = chap_content
         chap_list.append(chap)
         i += 1
     content_html(root_path, en_name, chap_list)
+    top_info.append(top)
 ncx(root_path, u'金庸修订版', ncx_list)
 opf(root_path, dc_info, item_info)
 top_html(root_path, u'金庸修订版', top_info, True)
