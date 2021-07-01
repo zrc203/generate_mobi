367859f01a11137e939112f1407923d18fa2e0f8
Rock Zhao <rockz@synnex.com>
2021-07-01 18:47:47 +0800

    a

--- down_novel.py
+++ down_novel.py
@@ -49,13 +49,12 @@ class Down(threading.Thread):
             chap['chap_name'] = chap_name
             chap['idx'] = chap_url_soup.idx
             print(chap_name)
-            chap_content = []
             content_list = content_soup.select('[id="content"] p')
-            for chap_soup in content_list:
-                chap_content.append(chap_soup.text)
-            chap['content'] = chap_content[0:-1]
+            chap_content = list(map(lambda x:x.text, content_list))
+            chap['content'] = chap_content
             chap['url'] = chap_url_soup['href']
             self.chap_list.append(chap)
+
 
 def get_new_soup(top_soup_list):
     i = 0
@@ -79,19 +78,19 @@ def get_new_soup(top_soup_list):
 
 
 if __name__ == '__main__':
-    noveurl = 'https://www.d586.com/4414/'
-    rootUrl = 'https://www.d586.com'
+    noveurl = 'https://www.dstiejuan.com/book/30113/'
+    rootUrl = 'https://www.dstiejuan.com'
     top_soup = get_content(noveurl)
-    novel_name = top_soup.select('[property="og:novel:book_name"]')[0]['content']
+    novel_name = top_soup.select_one('h1').text
     en_name = 'qugekongjie'
     root_path = 'D:\\jin\\' + novel_name
     if not os.path.exists(root_path):
         os.mkdir(root_path)
-    top_soup_list = top_soup.select('[id="list"] a')[13:]
+    top_soup_list = top_soup.select('dd a')[12:]
     dc_info = dict()
     dc_info['Title'] = novel_name
     dc_info['Language'] = 'zh-CN'
-    dc_info['Creator'] = top_soup.select('[property="og:novel:author"]')[0]['content']
+    dc_info['Creator'] = top_soup.select_one('h2 a').text
     dc_info['Copyrights'] = 'zrc'
     dc_info['Publisher'] = 'zrc'
     chap_list = get_new_soup(top_soup_list)
